efeb034bf710a10e55d3de98c0e2393cb0b90ae4
lrmendes98 <lrmmendes98@gmail.com>
2021-08-23 13:33:53 +0100

    probable fix pauliX, needs more testing

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,9 @@ int main(int argc, char* argv[])
 	QubitLayer qL(3);
 
 	// Apply pauliX gate to the first qubit
-	// qL.pauliX(2);
+	qL.pauliX(2);
 	qL.pauliX(1);
+	qL.pauliX(0);
 
 	// qL.hadamard(1);
 	// qL.hadamard(1);
@@ -24,8 +25,13 @@ int main(int argc, char* argv[])
 	// qL.hadamard(2);
 	// qL.pauliZ(1);
 
-	qL.printStateVector();
+	// qL.printStateVector();
 	qL.measure();
+
+	bitset<numQubits> state = 2;
+	cout << state << endl;
+	state.flip(1);
+	cout << state << endl;
 
 	return 0;
 }
