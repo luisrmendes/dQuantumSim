c4883da7f04294cdcc68d7f1b508fbf801453625
lrmendes98 <lrmmendes98@gmail.com>
2021-09-15 13:38:25 +0000

    remove another warning

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,6 @@ int main(int argc, char* argv[])
 		cout << "Warning: qubitCount != constexpr numQubits!" << endl;
 
 	int rank, size;
-	MPI_Status status;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
