542e7cac37b72dd2c118fa3111be9359d73c46ea
lrmendes98 <lrmmendes98@gmail.com>
2021-10-06 13:32:43 +0000

    cleanup

--- include/parser.h
+++ include/parser.h
@@ -5,7 +5,9 @@
 #include <vector>
 
 std::vector<unsigned int> sourceParser(char* fileName);
+
 int checkValidInstruction(std::string line);
+
 unsigned int getCodeOfInstruction(std::string line);
 
 #endif
--- scripts/q4Tests.sh
+++ scripts/q4Tests.sh
@@ -8,6 +8,4 @@ python scripts/test.py myqasmTests/q4_test1.myqasm 4 "0 100 50 100"
 python scripts/test.py myqasmTests/q4_test2.myqasm 4 "50 100 0 0"
 python scripts/test.py myqasmTests/q4_test3.myqasm 4 "50 50 100 0"
 
-
-
 exit 0
