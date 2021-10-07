cc9013fcedbfbaad496164afb4d227780e98535e
lrmendes98 <lrmmendes98@gmail.com>
2021-10-07 11:09:49 +0000

    add 5 qubit test

--- scripts/allTests.sh
+++ scripts/allTests.sh
@@ -9,10 +9,13 @@ make clean && make
 python scripts/test.py myqasmTests/q3_grovers.myqasm 3 "50 50 0"
 python scripts/test.py myqasmTests/q3_test1.myqasm 3 "100 50 100"
 
-# 4 qubts tests
+# 4 qubit tests
 python scripts/test.py myqasmTests/q4_test1.myqasm 4 "0 100 50 100"
 python scripts/test.py myqasmTests/q4_test2.myqasm 4 "50 100 0 0"
 python scripts/test.py myqasmTests/q4_test3.myqasm 4 "50 50 100 0"
+
+# 5 qubit tests
+python scripts/test.py myqasmTests/q5_test1.myqasm 4 "100 0 0 0 0"
 
 # 26 qubit tests
 python scripts/test.py myqasmTests/q26_test1.myqasm 26 "100 100 100 100 100 100 \
