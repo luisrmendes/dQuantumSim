0e273171180d48154ff55914a887bb10dbac7236
lrmendes98 <lrmmendes98@gmail.com>
2022-03-31 13:39:53 +0000

    remoivng output logs, add q26 hadamard test

--- include/flags.h
+++ include/flags.h
@@ -15,12 +15,14 @@
 // #define PAULIX_DEBUG_LOGS
 // #define HANDLER_STATES_DEBUG
 
+/*
 #define OUTPUT_LOGS                                                                 \
 	TOFFOLI_DEBUG_LOGS || CONTROLLEDZ_DEBUG_LOGS || CONTROLLEDX_DEBUG_LOGS ||       \
 		HADAMARD_DEBUG_LOGS || PAULIZ_DEBUG_LOGS || PAULIY_DEBUG_LOGS ||            \
 		PAULIX_DEBUG_LOGS || HANDLER_STATES_DEBUG || RECEIVED_OPERATION_LOGS ||     \
 		INSTRUCTIONS_HANDLER_LOGS || MEASURE_DEBUG_LOGS ||                          \
 		GET_STATE_LAYER_INFO_DEBUG_LOGS || MEASURE_STATE_VALUES_DEBUG_LOGS
+*/
 
 /**
  * Outras macros
