2e187e902930d15f4f19eae537bde2976e124469
lrmendes98 <lrmmendes98@gmail.com>
2022-08-10 20:00:00 +0000

    add documentation on parser

--- include/parser.hpp
+++ include/parser.hpp
@@ -4,10 +4,29 @@
 #include <string>
 #include <vector>
 
+/**
+ * @brief Convert quantum assembly instructions to integer codes.
+ * Parses a subset of OpenQASM 2.0 specification.
+ * 
+ * @param fileName 
+ * @return std::vector<unsigned int> 
+ */
 std::vector<unsigned int> sourceParser(char* fileName);
 
+/**
+ * @brief Returns the number of target qubits per instructions.
+ * 
+ * @param gate 
+ * @return int 
+ */
 int getNumTargetQubitsOfGate(std::string gate);
 
+/**
+ * @brief Returns the integer code of the quantum assembly instruction.
+ * 
+ * @param line 
+ * @return unsigned int 
+ */
 unsigned int getCodeOfInstruction(std::string line);
 
 #endif
